c415239f2903bebd6a942101f539caba6b7e648a
Hemant Chauhan <hemantchauhan1020@gmail.com>
2022-08-29 21:57:03 +0530

    initial commit

---
+++ final_project.js
@@ -0,0 +1,192 @@
+// gsap.registerPlugin(ScrollTrigger);
+// function locomoctive(){
+
+//     const locoScroll = new LocomotiveScroll({
+//       el: document.querySelector("#main"),
+//       smooth: true,
+//       tablet: { smooth: true },
+//       smartphone: { smooth: true }
+//     });
+//     locoScroll.on("scroll", ScrollTrigger.update);
+
+//     // locoScroll.on("scroll", function(dets){
+//     //   if(dets.direction === "up"){
+//     //     document.querySelector("nav").style.top = "0%";
+//     //   }
+//     //   else{
+//     //     document.querySelector("nav").style.top = "-100%"
+//     //   }
+//     // })
+
+//     ScrollTrigger.scrollerProxy("#main", {
+//       scrollTop(value) {
+//         return arguments.length
+//           ? locoScroll.scrollTo(value, 0, 0)
+//           : locoScroll.scroll.instance.scroll.y;
+//       },
+//       getBoundingClientRect() {
+//         return {
+//           top: 0,
+//           left: 0,
+//           width: window.innerWidth,
+//           height: window.innerHeight
+//         };
+//       }
+//     });
+
+
+//     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+//     ScrollTrigger.refresh();
+
+// };
+
+
+function locomotive() {
+  gsap.registerPlugin(ScrollTrigger);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+    smartphone: { smooth: true },
+    getDirection: true
+  });
+
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight
+      };
+    },
+    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+};
+
+
+
+function slidesHandle() {
+  let allSlides = document.querySelectorAll(".sld");
+  allSlides = [...allSlides];
+
+  var isPlaying = null;
+  allSlides.forEach(function (elm) {
+    elm.addEventListener("mouseover", function (dets) {
+      isPlaying = "#curtain" + dets.target.dataset.index;
+      document.querySelector(isPlaying).style.width = "100%";
+      document.querySelector("#curcularImg").style.display = "none";
+    })
+    elm.addEventListener("mouseleave", function (dets) {
+      document.querySelector(isPlaying).style.width = "0%";
+      document.querySelector("#curcularImg").style.diplay = "initial";
+    })
+  });
+
+
+
+  const circle = document.querySelector("#circularImg");
+  circle.addEventListener("mousemove", function (dets) {
+    const bndVal = document.querySelector("#circularImg").getBoundingClientRect();
+    const Xval = dets.clientX - bndVal.x;
+    const Yval = dets.clientY - bndVal.y;
+    document.querySelector("#dot").style.top = Yval + "px";
+    document.querySelector("#dot").style.left = Xval + "px";
+    document.querySelector("#dot").style.backgroundColor = "rgb(219, 255, 0)";
+    document.querySelector("#dot").style.transform = "scale(1.3)";
+    document.querySelector("#dot").style.boxShadow = "0 0 12px 1px rgb(219, 255, 0)";
+  });
+
+  circle.addEventListener("mouseleave", function (details) {
+    document.querySelector("#dot").style.top = "50%";
+    document.querySelector("#dot").style.left = "50%";
+    document.querySelector("#dot").style.backgroundColor = "white";
+    document.querySelector("#dot").style.transform = "scale(1)"
+    document.querySelector("#dot").style.boxShadow = "none";
+
+  });
+};
+
+
+
+
+
+function animationCode() {
+  const tl = gsap.timeline();
+  tl.to("#twoSlides .workSlide", {
+    scrollTrigger: {
+      trigger: "#twoSlides #workText",
+      scroller: "#main",
+      scrub: 1,
+      pin: true
+    },
+    duration: 10000,
+    top: "10%",
+    stagger: 1000000,
+  })
+    .to(".allCircle", {
+      scrollTrigger: {
+        trigger: ".allTxt",
+        scroller: "#main",
+        pin: true,
+        scrub: 1
+      },
+      top: "75%",
+      backgroundColor: "rgb(219, 255, 0)",
+      color: "black"
+    })
+    .to(".allTxt", {
+      scrollTrigger: {
+        trigger: ".allCircle",
+        scroller: "#main",
+        start: "top 100%",
+        end: "top 0%",
+        scrub: 0,
+      },
+      duration: 0.01,
+      color: "white",
+      backgroundColor: "black"
+    }, "-=10s")
+    .to("#allWork", {
+      scrollTrigger: {
+        trigger: ".allCircle",
+        scroller: "#main",
+        start: "top 100%",
+        end: "top 0%",
+        scrub: 0,
+      },
+      duration: 0.01,
+      color: "white",
+      backgroundColor: "black"
+    }, "-=10s")
+    .to(".allTxt h1", {
+      scrollTrigger: {
+        trigger: ".allCircle",
+        scroller: "#main",
+        start: "top 100%",
+        end: "top 0%",
+        scrub: 0,
+      },
+      duration: 0.01,
+      color: "white",
+    }, "-=10s");
+
+};
+
+
+
+
+locomotive();
+slidesHandle();
+animationCode();
